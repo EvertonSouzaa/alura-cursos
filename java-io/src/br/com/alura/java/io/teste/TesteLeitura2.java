616cbfd7fad407a984e327ba78a9f1227440716e
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-05-04 16:51:20 -0300

    Java parte 7: Trabalhando com java.io - Aula 04 - 'Formatação de valores'

---
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -0,0 +1,45 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner scanner = new Scanner(new File("contas.csv"));
+
+		// enquanto tiver linhas, imprime
+		while (scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+//			System.out.println(linha);
+
+			// Separando os valores usando SCANNER
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US); // metodo para nao da excecao de . ou , em diferentes regioes
+			linhaScanner.useDelimiter(","); // pattern ou regex para dizer ao scanner as separacoes
+
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
+			
+			// Formatação, quando precisar, olhar na DOCUMENTAÇÃO
+			System.out.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s: %08.2f %n", 
+					tipoConta, agencia, numero, titular, saldo);
+
+			linhaScanner.close();
+
+			// Separando os valores usando SPLIT que recebe um REGEX(expressao regular)
+//			String[] valores = linha.split(",");
+//			System.out.println(Arrays.toString(valores));
+//			System.out.println(valores[3]); // acessa o titular da conta
+
+		}
+		scanner.close();
+
+	}
+
+}
